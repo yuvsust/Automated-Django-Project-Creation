ca5a5c4a79bff7ee850e0616f25b2acc6ea8e479
Tanvir Hossain Yuvraz <33263219+yuvsust@users.noreply.github.com>
2021-08-06 16:14:34 +0600

    Handled Invalid Project Name error
    If Project name is invalid then no folder will be created and terminated the program

--- create-project-django.py
+++ create-project-django.py
@@ -18,14 +18,16 @@ print(" Installing Django ........")
 os.system("pip install django")
 print(" Initiating Django Project ........")
 os.chdir(current_dir)
-os.system('django-admin startproject ' + project_name)
-os.chdir(current_dir)
+resp = os.system('django-admin startproject ' + project_name)
 
-# Now move the venv folder to the project folder
-project_dir = current_dir + "//" + project_name
-venv_dir = current_dir + "//venv"
-shutil.move(venv_dir, project_dir + "//venv")
+if resp == 0:
+    os.chdir(current_dir)
 
-# Now rename the project directory with a additional '-Project' string for coolness
-print(" Done\n Renaming the Parent Folder ........\n Done")
-os.rename(project_name, project_name+"-Project")
+    # Now move the venv folder to the project folder
+    project_dir = current_dir + "//" + project_name
+    venv_dir = current_dir + "//venv"
+    shutil.move(venv_dir, project_dir + "//venv")
+
+    # Now rename the project directory with a additional '-Project' string for coolness
+    print(" Done\n Renaming the Parent Folder ........\n Done")
+    os.rename(project_name, project_name+"-Project")
